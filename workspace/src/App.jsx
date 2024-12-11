d8e0300d93bd55b209e85a0f3529414b4714f74e
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-11 09:12:11 +0900

    #5 Vite 및 CSS Module 정리

--- workspace/src/App.jsx
+++ workspace/src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import Button from "./Buttons";
+
+import styles from "./App.module.css";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1 className={styles.title}>Welcome Back!</h1>
+      <Button text={"Continue"} />
+    </div>
+  );
 }
 
 export default App;
